94da8aa9cc65191a51c682a81e46f63dc4ac2287
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-25 16:15:38 +0100

    map title, id , image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { db } from "@/src/db";
 import { projects } from "@/src/db/schema";
+import Image from "next/image";
+
 
 export default async function ProjectsPage() {
   const allProjects = await db.select().from(projects)
   return (
     <main>
-      <h1>ADAVERSE</h1>
       {allProjects.map((projects)=>(
         <div key={projects.id}>
           <h2>{projects.title}</h2>
+          {/* <img src={projects.urlImage}/> */}
         </div>
       ))}
     </main>
