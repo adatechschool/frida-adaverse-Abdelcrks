a13f7a8b94b0e347094b1930c9d26fcf92d29c8c
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-27 10:03:45 +0100

    add labels

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -1,25 +1,43 @@
 "use client"
 
-export const ProjectForm = () => {
+import { useState } from "react"
+
+
+
+
+
+export const ProjectForm = ({onClose}: {onClose :() => void}) => {
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <div className="flex justify-center">
-            <form action=""
+            <form action="" onSubmit={handleSubmit}
             className="relative mt-30 p-10 w-[450px] bg-white text-black flex flex-col items-center gap-10 rounded-2xl"
             >
-                <button className="absolute left-4 top-4 text-red-600 text-xl">❌</button>
+                <button className="absolute left-4 top-4 text-red-600 text-xl"
+                onClick={onClose}
+                >❌</button>
                 <h1 className="text-2xl">Publier un projet</h1>
 
-                <input type="text" name="title" placeholder="Quizz Marvel" />
-                <input type="url" name="github" placeholder="www.github.com" />
-                <input type="url" name="demo" placeholder="www.quizz-marvel.vercel.app" />
+                <label htmlFor="title">Titre</label>
+                <input type="text" id="title" name="title" placeholder="Quizz Marvel" />
+                <label htmlFor="github">Lien Github</label>
+                <input type="url" id="github"name="github" placeholder="www.github.com" />
+                <label htmlFor="demo">Lien Démo</label>
+                <input type="url" id="demo" name="demo" placeholder="www.quizz-marvel.vercel.app" />
 
-                <select name="promo">
+                <label htmlFor="promo">Promo</label>
+                <select name="promo" id="promo">
                     <option value="">Frida</option>
                     <option value="">Graces</option>
                 </select>
 
-                <select name="theme">
+                <label htmlFor="theme">Catégorie</label>
+                <select name="theme" id="theme">
                     <option value="">Quizz</option>
                     <option value="">Dataviz</option>
                     <option value="">Adacheckevent</option>
