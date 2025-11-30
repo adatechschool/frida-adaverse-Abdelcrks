d2af5090e7dac3683b1f4e105c61d35f93fc88c7
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-30 14:28:19 +0100

    add length categories

--- app/components/AllCategories.tsx
+++ app/components/AllCategories.tsx
@@ -40,7 +40,7 @@ export const AllCategories = ({categories, projects}:AllCategoriesProps) => {
                 )
                 return (
                     <section key={category.id} className="px-4 sm:px-6 lg:px-8 mt-10">
-                        <h2 className="text-2xl font-semibold">{category.name} ({})</h2>
+                        <h2 className="text-2xl font-semibold">{category.name} ({projectsOfCategory.length})</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-4">
                         {/* <div className="mt-4 p-10  grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-15"> */}
                             {projectsOfCategory.map((project) => (
