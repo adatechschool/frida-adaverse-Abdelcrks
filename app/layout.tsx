ec1b6da9f12b3673c146cf17c61d9fcc4433d09d
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-11-30 19:04:27 +0100

    layout goin async layout add select categories and composant and add props at Header componant

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/Header";
+import { categories } from "@/src/db/schema";
+import { AllCategories } from "./components/AllCategories";
+import { db } from "@/src/db";
+
 
 export const poppins = Poppins({
   subsets: ["latin"],
@@ -15,16 +19,19 @@ export const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const allCategories = await db.select().from(categories);
+
   return (
     <html lang="en">
-      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white`}>
-        <Header/>
-        <main className="">
+      <body className={`${poppins.variable} bg-[linear-gradient(356deg,rgba(0,0,0,1)_0%,rgba(78,56,56,1)_100%,rgba(0,0,0,1)_100%)] text-white max-w-6xl mx-auto`}>
+        <Header categories={allCategories}/>
+        <main className="min-h-screen w-full">
         <div className="w-full max-w-6xl mx-auto">
           {children}
         </div>
