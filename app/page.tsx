f0ebf28db9aa1136795b2e908525a52fa427b2cd
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-01 09:16:44 +0100

    add inner join for promo.name

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 
 import { db } from "@/src/db";
-import { categories, projects } from "@/src/db/schema";
+import { categories, projects, promos } from "@/src/db/schema";
 import { AllCategories } from "./components/AllCategories";
+import { isNotNull, eq, desc } from "drizzle-orm";
+
 
 
 export default async function ProjectsPage() {
   const allCategories = await db.select().from(categories)
-  const allProjects = await db.select().from(projects)
+  const allProjects = await db
+    .select({
+      id: projects.id,
+      title:projects.title,
+      slug:projects.slug,
+      urlGithub:projects.urlGitHub,
+      urlDemo:projects.urlDemo,
+      urlImage:projects.urlImage,
+      categoryId:projects.categoryId,
+      promoId:projects.promoId,
+      createdAt:projects.createdAt,
+      publishedAt: projects.publishedAt,
+      promoName: promos.name,
+    })
+    .from(projects)
+    .innerJoin(promos, eq(promos.id, projects.promoId))
+    .where(isNotNull(projects.publishedAt))
+    .orderBy(desc(projects.publishedAt))
 
   return (
 
