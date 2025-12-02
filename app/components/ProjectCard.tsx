60f1157afe32adc52c16ce3ec2f9783fc4ef8854
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-02 01:28:17 +0100

    change style light & darkmode

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -19,7 +19,6 @@ export const ProjectCard = ({project}:{project:projectCardProps}) => {
      const image = project.urlImage ?? "/images/default-thumbnail.png"
     return (
         <Link href={`/projects/${project.slug}`} className="block w-full max-w-2xl">
-            <div className="">
             <article 
             className="group relative w-full h-82 sm:h72 lg:h-96 overflow-hidden cursor-pointer shadow-2xl rounded-xl"
             // style={{minWidth: '600px', maxWidth: '1200px'}} 
@@ -29,15 +28,14 @@ export const ProjectCard = ({project}:{project:projectCardProps}) => {
                 className="absolute inset-0 bg-cover bg-center transition-transform duration-600 ease-out group-hover:scale-120 "
                 style={{backgroundImage: `url(${image})`}}
                 >
-                <p className="absolute top-3 right-4 bg-orange-500 px-3 py-1 rounded">{project.promoName.toUpperCase()}</p>
+                <p className="absolute top-3 right-4 bg-[linear-gradient(135deg,#6A00FF_0%,#B245FC_40%,#00A8FF_100%)] lg:text-lg sm:text-sm px-3 py-1 rounded">{project.promoName.toUpperCase()}</p>
                 </div>
                 <div 
                 className="absolute inset-0 bg-linear-to-t from-black/30 via-black/30"/>
             </article>
-            </div>
-            <p>{project.title}</p>
+            <p className="text-black dark:text-white lg:text-[18px] sm:text-sm">{project.title}</p>
             {/* <p> debug slug {project.slug}</p> */}
-            <p className="text-zinc-300">Le {project.publishedAt}</p>
+            <p className="text-zinc-700 dark:text-zinc-300">Le {project.publishedAt}</p>
 
         </Link>
     )
