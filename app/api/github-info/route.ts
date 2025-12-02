02cba048bb4f582609124a88df361d56604f954b
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-02 09:30:06 +0100

    split parts url repo user, repo

--- app/api/github-info/route.ts
+++ app/api/github-info/route.ts
@@ -1,5 +1,15 @@
 import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-    return Response.json ({ message : "Ok reçu 5/5" })
+
+    const repoUrl = req.nextUrl.searchParams.get("url")
+
+    if (!repoUrl){
+        return Response.json ({error : "Mauvaise url"}, {status : 400})
+    }
+
+    const parts = repoUrl.split("github.com/")[1]
+    const [user, repo] = parts.split("/")
+
+    return Response.json ({ user:user, repo:repo })
 }
